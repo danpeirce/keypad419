c38d7e0a376802931f49a657dfdf3277d8e30e46
Dan Peirce <peirce.dan@gmail.com>
2023-07-19 02:28:35 -0700

    using new library

--- keypad_test0.ino
+++ keypad_test0.ino
@@ -4,48 +4,21 @@
 //   * PID419  Membrane 3x4 Matrix Keypad  <-- this modified example
 //                                             uses the Adafruit 419 membrane keypad
 
-// note moved simplified keyboard_config.h into the this ino file 
 
-#include "Adafruit_Keypad.h"
+#include "keypad_test0.h"
 
-// define your specific keypad here via PID
-#define KEYPAD_PID419
-
-// reversed the order of these pins so that the keybad flex PCB cable would
-//   not pass over the board when connected and top side of keypad could be 
-//   kept print side up.
-#define R1    8
-#define R2    7
-#define R3    6
-#define R4    5
-#define C1    4
-#define C2    3
-#define C3    2
-
-const byte ROWS = 4; // rows
-const byte COLS = 3; // columns
-// define the symbols on the buttons of the keypads
-char keys[ROWS][COLS] = {
-    {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
-byte rowPins[ROWS] = {R1, R2, R3,
-                      R4};         // connect to the row pinouts of the keypad
-byte colPins[COLS] = {C1, C2, C3}; // connect to the column pinouts of the
-                                   // keypad
-
-//initialize an instance of class NewKeypad
-Adafruit_Keypad customKeypad = Adafruit_Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   Serial.begin(115200);   // will compile for a ESP32S2 which generally is set to 115200
-  customKeypad.begin();
+  Keypad419.begin();
 }
 
 void loop() {
 
-  customKeypad.tick();
+  Keypad419.tick();
 
-  while(customKeypad.available()){
-    keypadEvent e = customKeypad.read();
+  while(Keypad419.available()){
+    keypadEvent e = Keypad419.read();
     Serial.print((char)e.bit.KEY);
     if(e.bit.EVENT == KEY_JUST_PRESSED) Serial.println(" pressed");
     else if(e.bit.EVENT == KEY_JUST_RELEASED) Serial.println(" released");
